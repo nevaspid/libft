671064fec8c281d31c91e2fb7b01f8b850a8bb0b
nevaspid <109989668+nevaspid@users.noreply.github.com>
2022-11-09 17:28:21 +0100

    Add files via upload

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: nevaspid <romain.brendle.guido@gmail.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/06 20:24:05 by nevaspid          #+#    #+#             */
-/*   Updated: 2022/11/09 16:12:55 by nevaspid         ###   ########.fr       */
+/*   Updated: 2022/11/09 17:25:52 by nevaspid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,52 @@
 #include <stdio.h>
 #include <string.h>
 
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+	i++;
+	return (i);
+}
+
 size_t	ft_strlcpy(char *dest, const char *src, size_t lendest)
 {
 	size_t i;
 
 	i = 0;
-	while (dest[i] < lendest)
+	if (!src)
+	return (0);
+	if (lendest < ft_strlen(src))
 	{
-		dest[i] = src[i];
-		i++;
+		while (i < lendest && src[i] != '\0')
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		return (i);
 	}
-	dest[i] = '\0';
+	if (i < lendest && src[i] != '\0')
+	{
+		while (i < lendest && src[i] != '\0')
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
+		return (i);
+	}
 	return (i);
 }
 
-int	main(void)
-{
-	char src[50] = "1111";
-	char dest[60] = "22222";
 
-	printf("%lu, la vraie", strlcpy(dest, src, 20));
-	printf("%zu, la mienne", ft_strlcpy(dest, src, 20));
-}
+// int	main(void)
+// {
+// 	char src[50] = "maman aide moi";
+// 	char dest[60] = "stp";
+// 	printf("%s\n", dest);
+// 	//printf("%lu, la vraie\n", strlcpy(dest, src, 5));
+// 	printf("%zu, la mienne\n", ft_strlcpy(dest, src, 50));
+// 	printf("%s\n", dest);
+// }
