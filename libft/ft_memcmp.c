76de5ada10413ddbbc38984da8739e6d1d9b37c2
nevaspid <109989668+nevaspid@users.noreply.github.com>
2022-11-23 13:26:57 +0100

    Add files via upload

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: nevaspid <romain.brendle.guido@gmail.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 17:06:15 by nevaspid          #+#    #+#             */
-/*   Updated: 2022/11/17 17:18:21 by nevaspid         ###   ########.fr       */
+/*   Updated: 2022/11/18 17:01:24 by nevaspid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +23,10 @@ int ft_memcmp(const void *s1, const void *s2, size_t n)
 	str1 = (unsigned char *)s1;
 	str2 = (unsigned char *)s2;
 	i = 0;
-	while (i < n && (str1[i] != '\0' || str2[i] != '\0'))
+	while (i < n)
 	{
-		if (!ft_isascii(str1[i]) || !ft_isascii(str2[i]))
-			i++;
-		if (str1[i] > str2[i] || !str2[i])
-			return (1);
-		if (str1[i] < str2[i] || !str1[i])
-			return (-1);
+		if (str1[i] != str2[i])
+			return (str1[i] - str2[i]);
 		i++;
 	}
 	return (0);
